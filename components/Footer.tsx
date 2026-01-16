ce1b1334cfb30b45e2d1d9a1f8ce5d50898f7225
3dkane-9LMNTS <3dkane@gmail.com>
2026-01-16 03:10:06 -0500

    Update logo image source in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Instagram, Twitter, Linkedin, Mail, Shield } from 'lucide-react';
-import logoImage from 'logo.png';
+import logoImage from '7fa6b449af0658006c308e262ec964e0117dac44.png';
 
 interface FooterProps {
   onNavigate: (page: string, plan?: string) => void;
